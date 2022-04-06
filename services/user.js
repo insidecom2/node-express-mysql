54f97fdf17da8a327405928215bebf1f29665f3b
jakkrit <jakkritt26@gmail.com>
2022-04-06 15:29:33 +0700

    try catch

--- services/user.js
+++ services/user.js
@@ -4,11 +4,15 @@ const config = require('../config');
 async function getAll(page = 1) {
     const limit = 10;
     const offset = (page - 1) * [limit];
-    const rows = await db.query(
-        `SELECT * FROM user LIMIT ${offset},${limit}`
-      );
-    const data = !rows ? [] : rows;
-    return { status: 200, message:'success',data: data };    
+    try {
+        const rows = await db.query(
+            `SELECT * FROM user LIMIT ${offset},${limit}`
+          );
+        const data = !rows ? [] : rows;
+        return { status: 200, message:'success',data: data };    
+    } catch (error) {
+        return { status: 400, message:'cannot get data',data: [] };    
+    }
 }
 
 module.exports = {
